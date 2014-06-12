39a15a4df2d12802c3233216344717a672286630
tangbo124 <846452029@qq.com>
2014-06-12 15:03:23 +0800

    创建Mainviewcontroller文件

---
+++ test/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  test
+//
+//  Created by qianfeng on 14-6-12.
+//  Copyright (c) 2014年 唐波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
